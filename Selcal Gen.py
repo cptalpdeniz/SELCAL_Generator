6882c6fecb2b5a4947addc92bcf1f62b9398b312
Alp Deniz Şenyurt <cptalpdeniz@gmail.com>
2018-01-13 16:30:59 -0800

    Cleanup and performance improvements

--- Selcal Gen.py
+++ Selcal Gen.py
@@ -3,26 +3,19 @@ n = input('How many SELCALs would you like to generate? ')
 for i in range(n):
 	l = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'P', 'Q', 'R', 'S']
 	while True:
-		selcal1 = random.choice(l)
-		if (selcal1 == 'S'):
-			continue
-		else:
+		selcal1i = random.choice(range(len(l)))
+		selcal1 = l[selcal1i]
+		if (selcal1 != 'S'):															#check if first letter of the code is "S"
+			break
+	code = random.choice(l[selcal1i:])
+	selcal1 = selcal1[:] + code
+	while True:
+		selcal2i = random.choice(range(len(l)))
+		selcal2 = l[selcal2i]
+		if (selcal1[0] != selcal2 or selcal1[1] != selcal2 or selcal2 != 'S'):			#another check to make sure code is within the rules
 			break
 	while True:
-		code = random.choice(l) 
-		if (selcal1[0] < code):
-			selcal1 = selcal1[:] + code
-			break
-	while True:
-		selcal2 = random.choice(l)
-		if (selcal1[0] == selcal2 or selcal1[1] == selcal2):
-			continue
-		if (selcal2 == 'S'):
-			continue
-		else:
-			break
-	while True:
-		code = random.choice(l)
+		code = random.choice(l[selcal2i:])
 		if (selcal1[0] == code or selcal1[1] == code):
 			continue
 		if (selcal2[0] < code):
